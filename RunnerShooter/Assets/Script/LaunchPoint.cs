619a1bf8f05674669c39af8a3fa7400ed7a7fa25
Pft_Max <>
2024-06-17 15:00:11 +0200

    Script Movement + Script Shoot

---
+++ RunnerShooter/Assets/Script/LaunchPoint.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class LaunchPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject m_projectile;
+    [SerializeField] private float m_projectileSpeed;
+    [SerializeField] private float m_time;
+    private Vector3 m_mousePosition;
+    private Vector2 m_dir;
+    private float m_lookAngle;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_mousePosition = Input.mousePosition;
+        m_mousePosition = Camera.main.ScreenToWorldPoint(m_mousePosition);
+
+        Vector2 m_dir = new Vector2(m_mousePosition.x - transform.position.x, m_mousePosition.y - transform.position.y);
+
+        transform.up = m_dir;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+    }
+
+    private void Shoot()
+    {
+        GameObject _projectileClone = Instantiate(m_projectile);
+        _projectileClone.transform.position = transform.position;
+        _projectileClone.transform.rotation = Quaternion.Euler(0, 0, m_lookAngle);
+        _projectileClone.GetComponent<Rigidbody2D>().velocity = transform.up * m_projectileSpeed;
+        Destroy(_projectileClone, m_time);
+    }
+
+
+
+}
---
+++ RunnerShooter/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D m_rb2D;
+
+    [Header("-- Jump --")]
+    [SerializeField] private float m_jump;
+    [SerializeField] private float m_checkGroundRadius;
+    [SerializeField] private Transform m_groundCheckTransform;
+    [SerializeField] private LayerMask m_groundLayerMask;
+    private bool m_isGrounded;
+    [Header("-- Movement --")]
+    [SerializeField] private float m_speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_isGrounded = Physics2D.OverlapCircle(m_groundCheckTransform.position, m_checkGroundRadius, m_groundLayerMask);
+
+        Jump();
+        Movement();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && m_isGrounded)
+        {
+            m_rb2D.AddForce(new Vector2(m_rb2D.velocity.x, m_jump));
+        }
+    }
+    private void Movement()
+    {
+        var _inputX = Input.GetAxis("Horizontal");
+        m_rb2D.velocity = new Vector2(_inputX * m_speed, m_rb2D.velocity.y);
+    }
+
+
+}
